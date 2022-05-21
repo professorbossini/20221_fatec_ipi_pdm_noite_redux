4d70b5291b737c770f1e2daf26259b4ad84832c2
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-20 21:51:57 -0300

    apostila08-app-redux: fim da secao 2.10

--- index.js
+++ index.js
@@ -29,3 +29,42 @@ const solicitarCashback = (nome, valor) => {
         payload: {nome, valor}
     }
 }
+
+//arrow function
+//historicoDePedidosCashbackReducer
+//recebe uma fatia de estado sobre a qual operar (uma lista chamada historicoDePedidosCashback. Por padrão, ela é uma lista vazia)
+//recebe uma acao
+//devolve: uma lista que contém todos os pedidos da lista recebida e o pedido descrito no payload da ação
+//detalhe: somente operar no estado se o type for apropriado
+//detalhe2: obrigatório usar o spread (...)
+const historicoDePedidosCashbackReducer = (historicoDePedidosCashback = [], acao) => {
+    if (acao.type === "SOLICITAR_CASHBACK"){
+        return [
+            ...historicoDePedidosCashback, acao.payload
+        ]
+    }
+    return historicoDePedidosCashback  
+}
+//implementar o reducer que manipula o caixa
+//o caixa começa zerado
+function caixaReducer(valorEmCaixa = 0, acao){
+    if (acao.type === "SOLICITAR_CASHBACK")
+        return valorEmCaixa - acao.payload.valor
+    if (acao.type === "CRIAR_CONTRATO")
+        return valorEmCaixa + acao.payload.valor
+    return valorEmCaixa
+}
+//implementar o reducer que lida com a lista de contratos
+//ele pode criar contratos e cancelar contratos
+const contratosReducer = (listaDeContratosAtual = [], acao) => {
+    if (acao.type === "CRIAR_CONTRATO"){
+        return [
+            ...listaDeContratosAtual,
+            acao.payload
+        ]
+    }
+    if (acao.type === "CANCELAR_CONTRATO"){
+        return listaDeContratosAtual.filter(contrato => contrato.nome !== acao.payload.nome)
+    }
+    return listaDeContratosAtual
+}
